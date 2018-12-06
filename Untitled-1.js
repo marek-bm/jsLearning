7ee3ce1622a730de7906e4a21ec38a2c916b5c02
marek <marek.jastrzebski@outlook.com>
2018-12-06 18:35:34 +0100

    first script

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+var name;
+name="Marek";
+
+price=20;
+price2=5;
+
+price+price2;
+
---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+var name;
+name="Marek";
+
+price=20;
+price2=5;
+
+var total=price+price2;
+total2=price-price2;
